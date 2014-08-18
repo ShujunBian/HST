03836424b53d319592f61e183126a393eeaa49db
Emerosnwade <emerson931216@gmail.com>
2014-08-18 11:08:30 +0800

    修改第五个prototype 增加音乐和一个洞

--- HST/P5_Bell.h
+++ HST/P5_Bell.h
@@ -12,6 +12,16 @@
 @interface P5_Bell : CCNode
 
 typedef enum {
+    P5MusicDo = 1,
+    P5MusicRe,
+    P5MusicMi,
+    P5MusicFa,
+    P5MusicSol,
+    P5MusicLa,
+    P5MusicXi
+} P5MusicType;
+
+typedef enum {
     BellNormalMode,
     BellQuickMode,
 } BellMode;
@@ -23,11 +33,11 @@ typedef enum {
     PartBellHead
 } BellPart;
 
-
 @property (nonatomic, strong) CCSprite * bellBody;
 @property (nonatomic, strong) CCSprite * bellHead;
 @property (nonatomic, strong) CCSprite * bellMouth;
 @property (nonatomic, strong) CCSprite * bellEye;
+@property (nonatomic) P5MusicType currentMusicType;
 @property (nonatomic) BOOL isChoosen;
 
 - (ccColor3B)colorAtIndex:(NSUInteger)index;
